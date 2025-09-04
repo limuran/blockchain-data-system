ed879dc64dbff683f626efe81a310cd083721e53
limuran <limuran818@126.com>
2025-09-04 23:06:47 +0800

    编译合约

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,8 @@
-// const InfoContracts = artifacts.require('InfoContract')
+const InfoContracts = artifacts.require('InfoContract')
 const DataStorage = artifacts.require('DataStorage')
+// const RedPackage = artifacts.require('RedPackage')
 module.exports = function (deployer) {
   deployer.deploy(DataStorage)
+  // deployer.deploy(RedPackage)
+  deployer.deploy(InfoContracts)
 }
